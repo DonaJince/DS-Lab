24d327828c774fde353b78abc5dc969e17d42906
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-14 12:03:07 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-int vertex[20],cost[20][20],n;
+int vertex[20],cost[20][20],n,t[20][20],mincost=0,near[10],edgecount=0;
 int main()
 {
-	int i,j;
+	int i,j,k=0,l=0;
 	
 	//count of vertices
 	printf("enter the number of vertices:");
@@ -21,16 +21,23 @@ int main()
 	{
 		for(j=i+1;j<=n;j++)
 		{
-			if(i+1==j)
-			{
-				printf("enter the cost[%d][%d]:",vertex[i+1],vertex[j]);
-				scanf("%d",&cost[i+1][j]);
-			}
-			else if(i+1!=j)
+			if(i+1!=j)
 			{
 				printf("enter the cost[%d][%d]||cost[%d][%d]:",vertex[i+1],vertex[j],vertex[j],vertex[i+1]);
 				scanf("%d",&cost[i+1][j]);
 				cost[j][i+1]=cost[i+1][j];
+				if(cost[i+1][j]!=0)
+				{
+					edgecount++;
+					if(cost[i+1][j]<=mincost)
+					{
+						k=i+1;
+						j=j;
+						mincost=cost[i+1][j];
+						t[1,1]=k;
+						t[1][2]=j;
+					}
+				}
 			}
 		}
 	}
@@ -45,18 +52,20 @@ int main()
 		printf("\n");
 	}
 	
-	//setting the cost of  non exist nodes to a high value
-	for(i=0;i<=n;i++)
+	//setting the cost of  non existing nodes to a high value
+	for(i=1;i<=n;i++)
 	{
-		for(j=i+1;j<=n;j++)
+		for(j=i;j<=n;j++)
 		{
-			if(cost[i+1][j]==0&& cost[j][i+1]==0)
+			if(cost[i][j]==0&& cost[j][i]==0)
 			{
-			cost[i+1][j]=999;
-			cost[j][i+1]=999;
+			cost[i][j]=999;
+			cost[j][i]=999;
 			}
+			
 		}
 	}
+	//display cost matrix after setting the non exiting edgecost  to high value
 	for(i=1;i<=n;i++)
 	{
 		for(j=1;j<=n;j++)
@@ -65,5 +74,6 @@ int main()
 		}
 		printf("\n");
 	}
+	printf("%d",edgecount);
 	return 0;
 }
