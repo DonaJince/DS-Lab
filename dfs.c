0ff3339ad5beba1595b3c45b6e2e1fe2d62754d8
Dona Jince <146970847+DonaJince@users.noreply.github.com>
2023-11-28 11:55:45 +0530

    dfs.c

--- dfs.c
+++ dfs.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
-int s[10],visited[10],i,j,n,edge[10][10];
+int s[10],visited[10],i,j,n,edge[10][10],top=1,v;
+void push(int v)
+{
+	s[top]=v;
+	top++;
+}
+int pop()
+{
+	v= s[top];
+	top--;
+	return v;
+}
 int main()
 {
 	printf("Total no of vertices :: ");
@@ -18,13 +29,19 @@ int main()
 				scanf("%d",&edge[i][j]);
 				edge[j][i]=edge[i][j];
 			}
+			else
+			 	edge[j][i]=edge[i][j]=0;
 		}
 	}
+	//printf("\nselect a starting vertex from 1 to  %d:",n);
 	for(i=1;i<=n;i++)
 	{
 		for(j=1;j<=n;j++)
 		{
-			
+			if(edge[i][j]==1)
+			{
+				push(j);
+			}
 		}
 	}
 }
